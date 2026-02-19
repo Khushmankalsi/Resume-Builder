78af8de6592264f98aab4e6d312081e3067ddf0e
Khushman Singh Kalsi <kskalsi0108@gmail.com>
2026-02-19 21:39:17 +0530

    Feature branch conflicting change

--- forgot-password.php
+++ forgot-password.php
@@ -21,7 +21,7 @@
 
                     <div>
                         <h1 class="h3 fw-normal my-1"><b>Resume </b> Builder</h1>
-                        <p class="m-0">Forgot your password</p>
+                        <p class="m-0">Feature branch changed this line differently </p>
 
                     </div>
                 </div>
